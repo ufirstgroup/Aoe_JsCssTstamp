3003babfddf1f70981b3017c11aee62211075db0
Jean-Luc Geering <jlgeering.12@gmail.com>
2012-02-21 13:59:12 +0100

    update for magenta v1.10

--- app/code/local/Aoe/JsCssTstamp/Model/Package.php
+++ app/code/local/Aoe/JsCssTstamp/Model/Package.php
@@ -14,8 +14,8 @@ class Aoe_JsCssTstamp_Model_Package extends Mage_Core_Model_Design_Package {
         if (!$targetDir) {
             return '';
         }
-        if (Mage::helper('core')->mergeFiles($files, $targetDir . DS . $targetFilename, false, null, 'js')) {
-            return Mage::getBaseUrl('media') . 'js/' . $targetFilename;
+        if ($this->_mergeFiles($files, $targetDir . DS . $targetFilename, false, null, 'js')) {
+            return Mage::getBaseUrl('media', Mage::app()->getRequest()->isSecure()) . 'js/' . $targetFilename;
         }
         return '';
     }
@@ -26,14 +26,27 @@ class Aoe_JsCssTstamp_Model_Package extends Mage_Core_Model_Design_Package {
 	 * @return string
 	 */
      public function getMergedCssUrl($files) {
-    	$tstamp = $this->getYoungestFile($files);
-        $targetFilename = md5(implode(',', $files)) . '.' . $tstamp . '.css';
-        $targetDir = $this->_initMergerDir('css');
+        // secure or unsecure
+        $isSecure = Mage::app()->getRequest()->isSecure();
+        $mergerDir = $isSecure ? 'css_secure' : 'css';
+        $targetDir = $this->_initMergerDir($mergerDir);
         if (!$targetDir) {
             return '';
         }
-        if (Mage::helper('core')->mergeFiles($files, $targetDir . DS . $targetFilename, false, array($this, 'beforeMergeCss'), 'css')) {
-            return Mage::getBaseUrl('media') . 'css/' . $targetFilename; 
+
+        // base hostname & port
+        $baseMediaUrl = Mage::getBaseUrl('media', $isSecure);
+        $hostname = parse_url($baseMediaUrl, PHP_URL_HOST);
+        $port = parse_url($baseMediaUrl, PHP_URL_PORT);
+        if (false === $port) {
+            $port = $isSecure ? 443 : 80;
+        }
+
+        // merge into target file
+    	$tstamp = $this->getYoungestFile($files);
+        $targetFilename = md5(implode(',', $files) . "|{$hostname}|{$port}") . $tstamp . '.css';
+        if ($this->_mergeFiles($files, $targetDir . DS . $targetFilename, false, array($this, 'beforeMergeCss'), 'css')) {
+            return $baseMediaUrl . $mergerDir . '/' . $targetFilename;
         }
         return '';
      }
